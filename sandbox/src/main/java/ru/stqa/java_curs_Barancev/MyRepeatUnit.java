3c4c82725e512e220dffc039e1d143a11b011744
yafarkina <yafarkina@gmail.com>
2016-12-04 21:51:45 +0400

    добавлен метод hello

--- sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyRepeatUnit.java
+++ sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyRepeatUnit.java
@@ -3,7 +3,13 @@ package ru.stqa.java_curs_Barancev;
 public class MyRepeatUnit {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    hello("word") ;
+    hello("user") ;
+    hello("me") ;
   }
 
+ public static void hello (String samebody){
+   System.out.println("hello, " + samebody + "!");
+ }
+ 
 }
