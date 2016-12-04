f879972b25658ab0a150e4fa878e755276f6f111
yafarkina <yafarkina@gmail.com>
2016-12-04 22:13:39 +0400

    добавлена функция расчета площади прямоугольника

--- sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyRepeatUnit.java
+++ sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyRepeatUnit.java
@@ -6,10 +6,18 @@ public class MyRepeatUnit {
     hello("word") ;
     hello("user") ;
     hello("me") ;
+
+    int l=5;
+    System.out.println("площадь прямоугольника, со стороной " + l + " равна " + square(l));
   }
 
  public static void hello (String samebody){
    System.out.println("hello, " + samebody + "!");
  }
- 
+
+ public static int square (int l){
+   int s=l*l;
+   return s;
+ }
+
 }
