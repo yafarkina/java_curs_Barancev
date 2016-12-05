5220b519b9d13b4a4e67ce01b7c7ccab248ba502
yafarkina <yafarkina@gmail.com>
2016-12-05 22:28:17 +0400

    добавлены изменения в классы

--- sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyRepeatUnitClass.java
+++ sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyRepeatUnitClass.java
@@ -3,21 +3,23 @@ package ru.stqa.java_curs_Barancev;
 public class MyRepeatUnitClass {
 
   public static void main(String[] args) {
-    hello("word") ;
-    hello("user") ;
-    hello("me") ;
 
     Square s = new Square ();
     s.l=5;
     System.out.println("площадь квадрата, со стороной " + s.l + " равна " + square(s));
+
+    Rectangle r = new Rectangle ();
+    r.a=5;
+    r.b=6;
+    System.out.println("площадь прямоугольника, со сторонами " + r.a + " и "+ r.b + " равна " + square(r));
   }
 
- public static void hello (String samebody){
-   System.out.println("hello, " + samebody + "!");
- }
 
  public static double square (Square s){
-   return s.l*s.l;
+   return s.l * s.l;
  }
 
+ public static double square (Rectangle r){
+   return r.a * r.b;
+ }
 }
