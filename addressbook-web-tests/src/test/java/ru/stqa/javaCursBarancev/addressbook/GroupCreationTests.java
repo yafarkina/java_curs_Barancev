4238001e8904ec2b0f2b9d416c726e61288049ea
yafarkina <yafarkina@gmail.com>
2016-12-06 23:46:02 +0400

    рефакторинг теста - добавлен параметр

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/GroupCreationTests.java
@@ -4,18 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
-
 import java.util.concurrent.TimeUnit;
-import java.util.Date;
-import java.io.File;
 
-import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.*;
-
-import static org.openqa.selenium.OutputType.*;
 
 public class GroupCreationTests {
   FirefoxDriver wd;
@@ -26,16 +18,16 @@ public class GroupCreationTests {
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
 
     wd.get("http://localhost/addressbook/group.php");
-    login();
+    login("admin", "secret");
   }
 
-  private void login() {
+  private void login(String username, String password) {
     wd.findElement(By.name("user")).click();
     wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys("admin");
+    wd.findElement(By.name("user")).sendKeys(username);
     wd.findElement(By.name("pass")).click();
     wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys("secret");
+    wd.findElement(By.name("pass")).sendKeys(password);
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
@@ -43,7 +35,7 @@ public class GroupCreationTests {
   public void testGroupCreation() {
     gotoGroupPage();
     initGroupCreation();
-    fillGroupForm();
+    fillGroupForm("test1", "test2", "test3");
     submitGroupCreation();
     returnToGroupPage();
   }
@@ -56,16 +48,16 @@ public class GroupCreationTests {
     wd.findElement(By.name("submit")).click();
   }
 
-  private void fillGroupForm() {
+  private void fillGroupForm(String name, String header, String footer) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
-    wd.findElement(By.name("group_name")).sendKeys("test1");
+    wd.findElement(By.name("group_name")).sendKeys(name);
     wd.findElement(By.name("group_header")).click();
     wd.findElement(By.name("group_header")).clear();
-    wd.findElement(By.name("group_header")).sendKeys("test2");
+    wd.findElement(By.name("group_header")).sendKeys(header);
     wd.findElement(By.name("group_footer")).click();
     wd.findElement(By.name("group_footer")).clear();
-    wd.findElement(By.name("group_footer")).sendKeys("test3");
+    wd.findElement(By.name("group_footer")).sendKeys(footer);
   }
 
   private void initGroupCreation() {
