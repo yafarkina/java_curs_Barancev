ad05726ea3db28b7d65d5d085dc360856ef32911
yafarkina <yafarkina@gmail.com>
2016-12-06 09:45:16 +0400

    изменен класс point

--- sandbox/src/main/java/ru/stqa/java_curs_Barancev/Point.java
+++ sandbox/src/main/java/ru/stqa/java_curs_Barancev/Point.java
@@ -4,11 +4,11 @@ package ru.stqa.java_curs_Barancev;
  * Created by yafar_000 on 06.12.2016.
  */
 public class Point {
-  public double x;
-  public double y;
+  public double p1;
+  public double p2;
 
-  public Point (double x, double y){
-    this.x = x;
-    this.y = y;
+  public Point (double p1, double p2){
+    this.p1 = p1;
+    this.p2 = p2;
   }
 }
