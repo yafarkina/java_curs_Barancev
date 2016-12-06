2cd754f1438c4b5f6703f2d9a9ab1c0142492ff9
yafarkina <yafarkina@gmail.com>
2016-12-06 14:07:56 +0400

    изменено описание вызова класса и метода

--- sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyClassDistanceBetweenTwoPoints.java
+++ sandbox/src/main/java/ru/stqa/java_curs_Barancev/MyClassDistanceBetweenTwoPoints.java
@@ -4,9 +4,9 @@ public class MyClassDistanceBetweenTwoPoints {
 
   public static void main(String[] args) {
 
-    Point x= new Point (4, 8);
-    Point y= new Point(2, 6);
-    System.out.println("расстояние между двумя точками " + " A" + "(" + x.p1 + "," + y.p1 +")" + " и " + "B" + "(" + x.p2 + "," + y.p2 + ") " + " равно " + x.distance());
+    Point x= new Point (4.0, 8.0);
+    Point y= new Point(2.0, 6.0);
+    System.out.println("расстояние между двумя точками " + " A" + "(" + x.p1 + "," + y.p1 +")" + " и " + "B" + "(" + x.p2 + "," + y.p2 + ") " + " равно " + x.distance(y));
   }
 
  }
