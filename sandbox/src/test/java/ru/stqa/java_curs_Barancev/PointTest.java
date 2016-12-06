4a20bdf7c1e1d42c4d09c08c9355d0b98c50d3ac
yafarkina <yafarkina@gmail.com>
2016-12-06 14:56:55 +0400

    изменен тест расчета расстояния между точками

--- sandbox/src/test/java/ru/stqa/java_curs_Barancev/PointTest.java
+++ sandbox/src/test/java/ru/stqa/java_curs_Barancev/PointTest.java
@@ -9,8 +9,8 @@ import org.testng.annotations.Test;
 @Test
 public class PointTest {
   public void TestPoint (){
-    Point x = new Point (4.0, 2.0);
-    Point y = new Point (8.0, 6.0);
-    Assert.assertEquals(x.distance(y),  5.65);
+    Point x = new Point (4.0, 8.0);
+    Point y = new Point (2.0, 6.0);
+    Assert.assertEquals(x.distance(y),  5.656854249492381);
   }
 }
