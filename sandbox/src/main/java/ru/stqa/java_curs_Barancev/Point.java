531822ec9271958702ac0b5b423c086249d9004c
yafarkina <yafarkina@gmail.com>
2016-12-06 09:30:14 +0400

    создан объект point и описан конструктор

---
+++ sandbox/src/main/java/ru/stqa/java_curs_Barancev/Point.java
@@ -0,0 +1,14 @@
+package ru.stqa.java_curs_Barancev;
+
+/**
+ * Created by yafar_000 on 06.12.2016.
+ */
+public class Point {
+  public double x;
+  public double y;
+
+  public Point (double x, double y){
+    this.x = x;
+    this.y = y;
+  }
+}
