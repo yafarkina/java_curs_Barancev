2e8e9ae17aa4c424f0072900d1c40a08747fc58c
yafarkina <yafarkina@gmail.com>
2016-12-14 17:46:09 +0400

    класс ApplicationManadger  перенесен в пакет appmanager

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/ApplicationManadger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/ApplicationManadger.java
@@ -1,8 +1,11 @@
-package ru.stqa.javaCursBarancev.addressbook;
+package ru.stqa.javaCursBarancev.addressbook.appmanager;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import ru.stqa.javaCursBarancev.addressbook.EmailDataKontact;
+import ru.stqa.javaCursBarancev.addressbook.GroupData;
+import ru.stqa.javaCursBarancev.addressbook.PersonalDataKontact;
 
 import java.util.concurrent.TimeUnit;
 
@@ -21,11 +24,11 @@ public class ApplicationManadger {
     }
   }
 
-  protected void stop() {
+  public void stop() {
     wd.quit();
   }
 
-  protected void init() {
+  public void init() {
     wd = new FirefoxDriver();
     wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
@@ -42,15 +45,15 @@ public class ApplicationManadger {
     wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 
-  protected void returnToGroupPage() {
+  public void returnToGroupPage() {
     wd.findElement(By.linkText("group page")).click();
   }
 
-  protected void submitGroupCreation() {
+  public void submitGroupCreation() {
     wd.findElement(By.name("submit")).click();
   }
 
-  protected void fillGroupForm(GroupData groupData) {
+  public void fillGroupForm(GroupData groupData) {
     wd.findElement(By.name("group_name")).click();
     wd.findElement(By.name("group_name")).clear();
     wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
@@ -62,43 +65,43 @@ public class ApplicationManadger {
     wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
   }
 
-  protected void initGroupCreation() {
+  public void initGroupCreation() {
     wd.findElement(By.name("new")).click();
   }
 
-  protected void gotoGroupPage() {
+  public void gotoGroupPage() {
     wd.findElement(By.linkText("groups")).click();
   }
 
-  protected void deleteSelectedGroups() {
+  public void deleteSelectedGroups() {
       wd.findElement(By.name("delete")).click();
   }
 
-  protected void selectGroup() {
+  public void selectGroup() {
       wd.findElement(By.name("selected[]")).click();
   }
 
-  protected void gotoHomePage() {
+  public void gotoHomePage() {
       wd.findElement(By.linkText("home")).click();
   }
 
-  protected void submitKontactCreation() {
+  public void submitKontactCreation() {
       wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
   }
 
-  protected void fillKontactNotes(String notes) {
+  public void fillKontactNotes(String notes) {
       wd.findElement(By.name("notes")).click();
       wd.findElement(By.name("notes")).clear();
       wd.findElement(By.name("notes")).sendKeys(notes);
   }
 
-  protected void fillKontactAddress2(String address) {
+  public void fillKontactAddress2(String address) {
       wd.findElement(By.name("address2")).click();
       wd.findElement(By.name("address2")).clear();
       wd.findElement(By.name("address2")).sendKeys(address);
   }
 
-  protected void fillKontactEmail(EmailDataKontact emailDataKontact) {
+  public void fillKontactEmail(EmailDataKontact emailDataKontact) {
       wd.findElement(By.name("email")).click();
       wd.findElement(By.name("email")).clear();
       wd.findElement(By.name("email")).sendKeys(emailDataKontact.getEmail());
@@ -110,37 +113,37 @@ public class ApplicationManadger {
       wd.findElement(By.name("email3")).sendKeys(emailDataKontact.getEmail3());
   }
 
-  protected void fillKontactMobile(String mobile) {
+  public void fillKontactMobile(String mobile) {
       wd.findElement(By.name("mobile")).click();
       wd.findElement(By.name("mobile")).clear();
       wd.findElement(By.name("mobile")).sendKeys(mobile);
   }
 
-  protected void fillKontactAddress(String address) {
+  public void fillKontactAddress(String address) {
       wd.findElement(By.name("address")).click();
       wd.findElement(By.name("address")).clear();
       wd.findElement(By.name("address")).sendKeys(address);
   }
 
-  protected void fillKontactCompany(String company) {
+  public void fillKontactCompany(String company) {
       wd.findElement(By.name("company")).click();
       wd.findElement(By.name("company")).clear();
       wd.findElement(By.name("company")).sendKeys(company);
   }
 
-  protected void fillKontactTitle(String title) {
+  public void fillKontactTitle(String title) {
       wd.findElement(By.name("title")).click();
       wd.findElement(By.name("title")).clear();
       wd.findElement(By.name("title")).sendKeys(title);
   }
 
-  protected void fillKontactNickname(String nickname) {
+  public void fillKontactNickname(String nickname) {
       wd.findElement(By.name("nickname")).click();
       wd.findElement(By.name("nickname")).clear();
       wd.findElement(By.name("nickname")).sendKeys(nickname);
   }
 
-  protected void fillKontactFIO(PersonalDataKontact resonalDataKontact) {
+  public void fillKontactFIO(PersonalDataKontact resonalDataKontact) {
       wd.findElement(By.name("firstname")).click();
       wd.findElement(By.name("firstname")).clear();
       wd.findElement(By.name("firstname")).sendKeys(resonalDataKontact.getFirstname());
@@ -152,7 +155,7 @@ public class ApplicationManadger {
       wd.findElement(By.name("lastname")).sendKeys(resonalDataKontact.getLastname());
   }
 
-  protected void gotoKontactPage() {
+  public void gotoKontactPage() {
       wd.findElement(By.linkText("add new")).click();
   }
 }
