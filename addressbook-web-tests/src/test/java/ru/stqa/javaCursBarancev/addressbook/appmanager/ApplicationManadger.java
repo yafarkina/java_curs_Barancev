832c05ee174a765a34cc93d652b0f61961c91f9a
yafarkina <yafarkina@gmail.com>
2016-12-15 00:02:07 +0400

    создан класс KontactHelper

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/ApplicationManadger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/ApplicationManadger.java
@@ -1,10 +1,7 @@
 package ru.stqa.javaCursBarancev.addressbook.appmanager;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import ru.stqa.javaCursBarancev.addressbook.model.EmailDataKontact;
-import ru.stqa.javaCursBarancev.addressbook.model.PersonalDataKontact;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,6 +11,7 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManadger {
   FirefoxDriver wd;
 
+  private KontactHelper kontactHelper;
   private  NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
   private SessionHelper sessionHelper;
@@ -33,84 +31,13 @@ public class ApplicationManadger {
 
   public void init() {
     wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+   wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
     sessionHelper = new SessionHelper(wd);
+    kontactHelper = new KontactHelper(wd);
     sessionHelper.login("admin", "secret");
-  }
-
-  
-
-  public void submitKontactCreation() {
-    wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-  }
-
-  public void fillKontactNotes(String notes) {
-    wd.findElement(By.name("notes")).click();
-    wd.findElement(By.name("notes")).clear();
-    wd.findElement(By.name("notes")).sendKeys(notes);
-  }
-
-  public void fillKontactAddress2(String address) {
-   wd.findElement(By.name("address2")).click();
-    wd.findElement(By.name("address2")).clear();
-    wd.findElement(By.name("address2")).sendKeys(address);
-  }
-
-  public void fillKontactEmail(EmailDataKontact emailDataKontact) {
-    wd.findElement(By.name("email")).click();
-    wd.findElement(By.name("email")).clear();
-    wd.findElement(By.name("email")).sendKeys(emailDataKontact.getEmail());
-    wd.findElement(By.name("email2")).click();
-    wd.findElement(By.name("email2")).clear();
-    wd.findElement(By.name("email2")).sendKeys(emailDataKontact.getEmail2());
-    wd.findElement(By.name("email3")).click();
-   wd.findElement(By.name("email3")).clear();
-    wd.findElement(By.name("email3")).sendKeys(emailDataKontact.getEmail3());
-  }
-
-  public void fillKontactMobile(String mobile) {
-   wd.findElement(By.name("mobile")).click();
-    wd.findElement(By.name("mobile")).clear();
-    wd.findElement(By.name("mobile")).sendKeys(mobile);
-  }
-
-  public void fillKontactAddress(String address) {
-    wd.findElement(By.name("address")).click();
-    wd.findElement(By.name("address")).clear();
-    wd.findElement(By.name("address")).sendKeys(address);
-  }
-
-  public void fillKontactCompany(String company) {
-    wd.findElement(By.name("company")).click();
-    wd.findElement(By.name("company")).clear();
-    wd.findElement(By.name("company")).sendKeys(company);
-  }
-
-  public void fillKontactTitle(String title) {
-   wd.findElement(By.name("title")).click();
-    wd.findElement(By.name("title")).clear();
-    wd.findElement(By.name("title")).sendKeys(title);
-  }
-
-  public void fillKontactNickname(String nickname) {
-   wd.findElement(By.name("nickname")).click();
-    wd.findElement(By.name("nickname")).clear();
-    wd.findElement(By.name("nickname")).sendKeys(nickname);
-  }
-
-  public void fillKontactFIO(PersonalDataKontact resonalDataKontact) {
-    wd.findElement(By.name("firstname")).click();
-    wd.findElement(By.name("firstname")).clear();
-   wd.findElement(By.name("firstname")).sendKeys(resonalDataKontact.getFirstname());
-    wd.findElement(By.name("middlename")).click();
-    wd.findElement(By.name("middlename")).clear();
-    wd.findElement(By.name("middlename")).sendKeys(resonalDataKontact.getMiddlename());
-    wd.findElement(By.name("lastname")).click();
-    wd.findElement(By.name("lastname")).clear();
-    wd.findElement(By.name("lastname")).sendKeys(resonalDataKontact.getLastname());
   }
 
   public GroupHelper getGroupHelper() {
@@ -119,5 +46,9 @@ public class ApplicationManadger {
 
   public NavigationHelper getNavigationHelper() {
     return navigationHelper;
+  }
+
+  public KontactHelper getKontactHelper() {
+    return kontactHelper;
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactCreationTests.java
@@ -9,16 +9,16 @@ public class KontactCreationTests extends TestBase {
     @Test
     public void testKontactCreation() {
         app.getNavigationHelper().gotoKontactPage();
-        app.fillKontactFIO(new PersonalDataKontact("first name", "middle name", "last name"));
-        app.fillKontactNickname("nickname");
-        app.fillKontactTitle("title");
-        app.fillKontactCompany("company");
-        app.fillKontactAddress("address");
-        app.fillKontactMobile("+79999999999");
-        app.fillKontactEmail(new EmailDataKontact("test1@test1.test1", "test2@test2.test2", "test3@.test3.test3"));
-        app.fillKontactAddress2("address");
-        app.fillKontactNotes("notes");
-        app.submitKontactCreation();
+        app.getKontactHelper().fillKontactFIO(new PersonalDataKontact("first name", "middle name", "last name"));
+        app.getKontactHelper().fillKontactNickname("nickname");
+        app.getKontactHelper().fillKontactTitle("title");
+        app.getKontactHelper().fillKontactCompany("company");
+        app.getKontactHelper().fillKontactAddress("address");
+        app.getKontactHelper().fillKontactMobile("+79999999999");
+        app.getKontactHelper().fillKontactEmail(new EmailDataKontact("test1@test1.test1", "test2@test2.test2", "test3@.test3.test3"));
+        app.getKontactHelper().fillKontactAddress2("address");
+        app.getKontactHelper().fillKontactNotes("notes");
+        app.getKontactHelper().submitKontactCreation();
         app.getNavigationHelper().gotoHomePage();
     }
 }
