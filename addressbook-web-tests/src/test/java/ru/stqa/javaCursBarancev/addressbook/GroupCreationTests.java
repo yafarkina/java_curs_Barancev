062f32e3b9431f83106a1cdd265d30b0ddbcc0de
yafarkina <yafarkina@gmail.com>
2016-12-14 16:19:04 +0400

    удалено лишнее из импорта

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/GroupCreationTests.java
@@ -1,13 +1,7 @@
 package ru.stqa.javaCursBarancev.addressbook;
 
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.AfterMethod;
+
 import org.testng.annotations.Test;
-
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.*;
 
 public class GroupCreationTests extends TestBase {
 
