d75690ce8880e501a3716d589065d2d2043cc323
yafarkina <yafarkina@gmail.com>
2016-12-14 22:51:23 +0400

    перенесены тестовые методы в пакет тест

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/TestBase.java
@@ -1,4 +1,4 @@
-package ru.stqa.javaCursBarancev.addressbook;
+package ru.stqa.javaCursBarancev.addressbook.tests;
 
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
