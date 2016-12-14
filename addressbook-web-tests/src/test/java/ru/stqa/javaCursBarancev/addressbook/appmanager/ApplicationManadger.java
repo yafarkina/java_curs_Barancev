5ae3754a8630cabf01c293d6f3084bdd3382f0f8
yafarkina <yafarkina@gmail.com>
2016-12-14 23:50:00 +0400

    создан класс SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/ApplicationManadger.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/ApplicationManadger.java
@@ -16,6 +16,7 @@ public class ApplicationManadger {
 
   private  NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
+  private SessionHelper sessionHelper;
 
   public static boolean isAlertPresent(FirefoxDriver wd) {
     try {
@@ -36,18 +37,11 @@ public class ApplicationManadger {
     wd.get("http://localhost/addressbook/group.php");
     groupHelper = new GroupHelper(wd);
     navigationHelper = new NavigationHelper(wd);
-    login("admin", "secret");
+    sessionHelper = new SessionHelper(wd);
+    sessionHelper.login("admin", "secret");
   }
 
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-   wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
+  
 
   public void submitKontactCreation() {
     wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
---
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/SessionHelper.java
@@ -0,0 +1,24 @@
+package ru.stqa.javaCursBarancev.addressbook.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by yafar_000 on 14.12.2016.
+ */
+public class SessionHelper {
+  private FirefoxDriver wd;
+
+  public SessionHelper(FirefoxDriver wd) {
+    this.wd = wd;
+  }
+  public void login(String username, String password) {
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+}
