b034406ff9e97e3b5684297934e31328b811c091
yafarkina <yafarkina@gmail.com>
2016-12-15 15:59:29 +0400

    создание класса KontactModificationTests èÏî

---
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactModificationTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.javaCursBarancev.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by yafar_000 on 15.12.2016.
+ */
+public class KontactModificationTests extends TestBase{
+
+  @Test
+
+  public void testKontactModification() {
+
+  }
+}
