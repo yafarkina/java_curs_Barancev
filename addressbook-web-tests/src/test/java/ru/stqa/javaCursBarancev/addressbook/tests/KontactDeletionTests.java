1c3e7e296623aadd792f027f70004d93a014dac5
yafarkina <yafarkina@gmail.com>
2016-12-15 15:59:00 +0400

    создание класса KontactDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactDeletionTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.javaCursBarancev.addressbook.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by yafar_000 on 15.12.2016.
+ */
+public class KontactDeletionTests extends TestBase {
+
+  @Test
+
+  public void testKontactDeletion() {
+
+  }
+
+}
