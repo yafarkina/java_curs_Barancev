b013373f2ecd1a275abe401de1fe7b50532d8c8e
yafarkina <yafarkina@gmail.com>
2016-12-15 12:33:56 +0400

    рефакторинг класса SessionHelper

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/appmanager/SessionHelper.java
@@ -12,12 +12,8 @@ public class SessionHelper extends HelperBase {
     super(wd);
   }
   public void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
-  }
+    type(By.name("user"), username);
+    type(By.name("pass"), password);
+    clic(By.xpath("//form[@id='LoginForm']/input[3]"));
+   }
 }
