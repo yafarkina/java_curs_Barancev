9bbd6003834fa56e1ac7796292c5e64846f2e651
yafarkina <yafarkina@gmail.com>
2016-12-17 13:33:57 +0400

    добавлен конструктор KontactData

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/model/KontactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/model/KontactData.java
@@ -5,13 +5,55 @@ package ru.stqa.javaCursBarancev.addressbook.model;
  */
 public class KontactData {
 
-  public String group;
+  private String group;
+  private String address;
+  private String nickname;
+  private String title;
+  private String company;
+  private String mobile;
+  private String address2;
+  private String notes;
 
-  public KontactData (String group) {
+  public KontactData(String group, String address, String nickname, String title, String company, String mobile, String address2, String notes) {
     this.group = group;
+    this.address = address;
+    this.nickname = nickname;
+    this.title = title;
+    this.company = company;
+    this.mobile = mobile;
+    this.address2 = address2;
+    this.notes = notes;
   }
 
   public String getGroup() {
     return group;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getCompany() {
+    return company;
+  }
+
+  public String getMobile() {
+    return mobile;
+  }
+
+  public String getAddress2() {
+    return address2;
+  }
+
+  public String getNotes() {
+    return notes;
   }
 }
