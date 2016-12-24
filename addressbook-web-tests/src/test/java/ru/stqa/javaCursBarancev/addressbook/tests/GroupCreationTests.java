22cc4c3ce0d9bd75e5bc6f696c893f2bb39c3543
yafarkina <yafarkina@gmail.com>
2016-12-25 00:49:36 +0400

    для вычисления максим значения id реализовано лямбдо-выражение

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/GroupCreationTests.java
@@ -21,13 +21,14 @@ public class GroupCreationTests extends TestBase {
    // int after = app.getGroupHelper().getGroupCount();
     Assert.assertEquals(after.size(), befor.size() + 1);
 
-    int max = 0;
-    for (GroupData g: after){
-      if (g.getId() > max) {
-        max = g.getId();
-      }
-    }
-    group.setId(max);
+    //int max = 0;
+    //for (GroupData g: after){
+    //  if (g.getId() > max) {
+    //    max = g.getId();
+   //  }
+   // }
+
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     befor.add(group);
     Assert.assertEquals(new HashSet<Object>(befor),new HashSet<Object>(after) );
   }
