1266f21a4457cccab957d2c6cc216262b6bc6cf0
yafarkina <yafarkina@gmail.com>
2017-01-09 01:11:51 +0400

    реализована загрузка данных из файла

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactCreationTests.java
@@ -2,10 +2,11 @@ package ru.stqa.javaCursBarancev.addressbook.tests;
 
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import ru.stqa.javaCursBarancev.addressbook.model.GroupData;
 import ru.stqa.javaCursBarancev.addressbook.model.KontactData;
 import ru.stqa.javaCursBarancev.addressbook.model.Kontacts;
 
-import java.io.File;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,62 +17,33 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class KontactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validKontacts(){
-    List<Object[]> list = new ArrayList<Object[]>();
+  public Iterator<Object[]> validKontacts() throws IOException {
     File photo = new File("src/test/resources/листочек.png");
-    list.add(new Object[] {new KontactData()
-            .withFirstname("first_name")
-            .withMiddlename("middlename")
-            .withLastname("last_name")
-            .withNickname("nickname")
-            .withCompany("company")
-            .withTitle("title")
-            .withAddress("address")
-            .withHomePhone("(8-333)33-33-33")
-            .withMobile("+79999999999")
-            .withWorkPhone("22 22 22")
-            .withEmail("email@mail.mail")
-            .withEmail2("email2@mail.mail")
-            .withEmail3("email3@mail.mail")
-            .withPhoto(photo)
-            // .withGroup("test1")
-            .withAddress2("address2")});
+    List<Object[]> list = new ArrayList<Object[]>();
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/kontacts.csv")));
+    String line = reader.readLine();
+    while (line != null) {
+      String[] split = line.split(";");
+      list.add(new Object[] {new KontactData()
+              .withFirstname(split[0])
+              .withMiddlename(split[1])
+              .withLastname(split[2])
+              .withNickname(split[3])
+              .withCompany(split[4])
+              .withTitle(split[5])
+              .withAddress(split[6])
+              .withHomePhone(split[7])
+              .withMobile(split[8])
+              .withWorkPhone(split[9])
+              .withEmail(split[10])
+              .withEmail2(split[11])
+              .withEmail3(split[12])
+              .withPhoto(photo)
+              // .withGroup("test1")
+              .withAddress2(split[13])});
 
-    list.add(new Object[] {new KontactData()
-            .withFirstname("first_name2")
-            .withMiddlename("middlename2")
-            .withLastname("last_name2")
-            .withNickname("nickname2")
-            .withCompany("company2")
-            .withTitle("title2")
-            .withAddress("address")
-            .withHomePhone("(8-333)33-33-33")
-            .withMobile("+79999999999")
-            .withWorkPhone("22 22 22")
-            .withEmail("email@mail.mail")
-            .withEmail2("email2@mail.mail")
-            .withEmail3("email3@mail.mail")
-            .withPhoto(photo)
-            // .withGroup("test1")
-            .withAddress2("address2")});
-
-    list.add(new Object[] {new KontactData()
-            .withFirstname("first_name3")
-            .withMiddlename("middlename")
-            .withLastname("last_name3")
-            .withNickname("nickname3")
-            .withCompany("company3")
-            .withTitle("title3")
-            .withAddress("address")
-            .withHomePhone("(8-333)33-33-33")
-            .withMobile("+79999999999")
-            .withWorkPhone("22 22 22")
-            .withEmail("email@mail.mail")
-            .withEmail2("email2@mail.mail")
-            .withEmail3("email3@mail.mail")
-            .withPhoto(photo)
-            // .withGroup("test1")
-            .withAddress2("address2")});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
