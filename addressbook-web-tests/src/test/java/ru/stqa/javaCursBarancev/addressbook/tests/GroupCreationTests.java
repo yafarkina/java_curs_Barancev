65207e09d719d670057946e86fd697f575ef3bb3
yafarkina <yafarkina@gmail.com>
2017-01-09 00:39:51 +0400

    реализована параметриация тестов

--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/GroupCreationTests.java
@@ -1,20 +1,33 @@
 package ru.stqa.javaCursBarancev.addressbook.tests;
 
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.javaCursBarancev.addressbook.model.GroupData;
 import ru.stqa.javaCursBarancev.addressbook.model.Groups;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
 
-  @Test
-  public void testGroupCreation() {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
+    list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
+    list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) {
     app.goTo().GroupPage();
     Groups befor = app.Group().all();
-    GroupData group = new GroupData().withName("test1");
     app.Group().create(group);
     assertThat(app.Group().getGroupCount(), equalTo(befor.size() + 1));
     Groups after = app.Group().all();
--- addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/javaCursBarancev/addressbook/tests/KontactCreationTests.java
@@ -1,23 +1,25 @@
 package ru.stqa.javaCursBarancev.addressbook.tests;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.javaCursBarancev.addressbook.model.KontactData;
 import ru.stqa.javaCursBarancev.addressbook.model.Kontacts;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class KontactCreationTests extends TestBase {
 
-  @Test
-  public void testKontactCreation() {
-    app.goTo().HomePage();
-    Kontacts befor = app.Kontact().all();
+  @DataProvider
+  public Iterator<Object[]> validKontacts(){
+    List<Object[]> list = new ArrayList<Object[]>();
     File photo = new File("src/test/resources/листочек.png");
-    app.goTo().KontactPage();
-    KontactData kontact = new KontactData()
+    list.add(new Object[] {new KontactData()
             .withFirstname("first_name")
             .withMiddlename("middlename")
             .withLastname("last_name")
@@ -33,8 +35,51 @@ public class KontactCreationTests extends TestBase {
             .withEmail3("email3@mail.mail")
             .withPhoto(photo)
             // .withGroup("test1")
-            .withAddress2("address2");
+            .withAddress2("address2")});
 
+    list.add(new Object[] {new KontactData()
+            .withFirstname("first_name2")
+            .withMiddlename("middlename2")
+            .withLastname("last_name2")
+            .withNickname("nickname2")
+            .withCompany("company2")
+            .withTitle("title2")
+            .withAddress("address")
+            .withHomePhone("(8-333)33-33-33")
+            .withMobile("+79999999999")
+            .withWorkPhone("22 22 22")
+            .withEmail("email@mail.mail")
+            .withEmail2("email2@mail.mail")
+            .withEmail3("email3@mail.mail")
+            .withPhoto(photo)
+            // .withGroup("test1")
+            .withAddress2("address2")});
+
+    list.add(new Object[] {new KontactData()
+            .withFirstname("first_name3")
+            .withMiddlename("middlename")
+            .withLastname("last_name3")
+            .withNickname("nickname3")
+            .withCompany("company3")
+            .withTitle("title3")
+            .withAddress("address")
+            .withHomePhone("(8-333)33-33-33")
+            .withMobile("+79999999999")
+            .withWorkPhone("22 22 22")
+            .withEmail("email@mail.mail")
+            .withEmail2("email2@mail.mail")
+            .withEmail3("email3@mail.mail")
+            .withPhoto(photo)
+            // .withGroup("test1")
+            .withAddress2("address2")});
+    return list.iterator();
+  }
+
+  @Test(dataProvider = "validKontacts")
+  public void testKontactCreation(KontactData kontact) {
+    app.goTo().HomePage();
+    Kontacts befor = app.Kontact().all();
+    app.goTo().KontactPage();
     app.Kontact().create(kontact, false);
     app.goTo().HomePage();
     assertThat(app.Kontact().getKontactCount(), equalTo(befor.size() + 1));
